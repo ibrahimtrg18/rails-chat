6e58691fb159b406b2f71ae9817aa1a77405e325
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-23 14:28:06 +0700

    feat: migration db to have many to many users and rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_072314) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_072714) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
