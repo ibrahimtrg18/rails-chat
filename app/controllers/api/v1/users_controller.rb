5421337460ebfaa157fd18c4b470dffa48f1637c
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-23 14:51:10 +0700

    refactor: json response flow

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,11 +4,13 @@ class Api::V1::UsersController < ApplicationController
   # Skip CSRF protection for the create action
   skip_before_action :verify_authenticity_token, only: [:create]
 
+  # GET /api/v1/users
   def index
     @users = User.all
-    json_response(@users, 'Users retrieved successfully')
+    json_response(@users, "Successfully retrieved #{@users.count} rooms")
   end
 
+  # POST /api/v1/users
    # Create a new user
   def create
     # Create instance user from params
@@ -17,9 +19,9 @@ class Api::V1::UsersController < ApplicationController
     # Create user in the database
     if @user.save
       response_data = @user.as_json(except: [:password, :password_digest])
-      json_response(response_data, 'Succesfully create a new user', :created)
+      json_response(response_data, 'Succesfully create a new user', nil, :created)
     else
-      json_response(nil, user_params, @user.errors.full_messages, :unprocessable_entity)
+      json_response(nil, "Failed cause of #{@user.errors.full_messages.first}", :unprocessable_entity, @user.errors.full_messages)
     end
   end
 
--- app/helpers/response_helper.rb
+++ app/helpers/response_helper.rb
@@ -1,5 +1,5 @@
 module ResponseHelper
-  def json_response(data, message = nil, error = nil, status = :ok)
+  def json_response(data, message = nil, status = :ok, error = nil)
     render json: {data: data, message: message, error: error, status: status}, status: status
   end
 end
