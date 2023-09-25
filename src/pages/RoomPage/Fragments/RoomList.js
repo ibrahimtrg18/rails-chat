31c4cdb6e891b3c40c3b761a1c9b29a43c442d34
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-25 20:17:34 +0700

    feat: improving UI

--- src/pages/RoomPage/Fragments/RoomList.js
+++ src/pages/RoomPage/Fragments/RoomList.js
@@ -8,13 +8,16 @@ import {
   Button,
   useDisclosure,
   useToast,
+  Text,
 } from "@chakra-ui/react";
 import { useRoomContext } from "../../../contexts/RoomContext";
 import { Link } from "react-router-dom";
 import { axios } from "../../../libs/axios";
 import { RoomCreateModal } from "./RoomCreateModal";
+import { useAuthContext } from "../../../contexts/AuthContext";
 
 export const RoomList = () => {
+  const { user } = useAuthContext();
   const { rooms, initRooms } = useRoomContext();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
@@ -46,23 +49,34 @@ export const RoomList = () => {
         <Button onClick={onOpen}>Create Room</Button>
       </Flex>
       <Grid gap="4" templateColumns="repeat(3, 1fr)">
-        {rooms.map((room) => (
-          <Link to={`/rooms/${room.id}`}>
-            <GridItem>
-              <Box
-                key={room.id}
-                borderWidth="1px"
-                borderRadius="lg"
-                boxShadow="lg"
-                p="4"
-              >
-                <Heading fontSize="lg" textTransform="capitalize">
-                  {room.name}
-                </Heading>
-              </Box>
-            </GridItem>
-          </Link>
-        ))}
+        {rooms.map((room) => {
+          const usersCount = room.users.length;
+          const isJoined = room.users.some((_user) => _user.id === user.id);
+
+          return (
+            <Link to={`/rooms/${room.id}`}>
+              <GridItem>
+                <Flex
+                  key={room.id}
+                  justifyContent="space-between"
+                  flexWrap="wrap"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  boxShadow="lg"
+                  p="4"
+                  {...(isJoined && {
+                    bgColor: "blue.50",
+                  })}
+                >
+                  <Heading fontSize="lg" textTransform="capitalize">
+                    {room.name}
+                  </Heading>
+                  <Text fontSize="sm">{usersCount} users</Text>
+                </Flex>
+              </GridItem>
+            </Link>
+          );
+        })}
       </Grid>
     </Box>
   );
