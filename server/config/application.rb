8d6523b6a00404ac643b6b28e6a3eecd8c1c31d5
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-26 02:28:56 +0700

    fix: cable

--- server/config/application.rb
+++ server/config/application.rb
@@ -21,5 +21,6 @@ module RailsChat
 
     # Configure for rails cable
     config.action_cable.mount_path = '/cable'
+    config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://', nil]
   end
 end
