08092cb5d10f7bfde04d9730c7377dc06da4f4fe
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-26 01:29:37 +0700

    fix: db migrate

--- server/app/models/create_room_user.rb
+++
@@ -1,2 +0,0 @@
-class CreateRoomUser < ApplicationRecord
-end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_and_belongs_to_many :rooms
-  has_many :rooms_users
-  has_many :joined_rooms, through: :rooms_users, source: :room
+  has_many :room_users
+  has_many :joined_rooms, through: :room_users, source: :room
 
   # Validations
   validates :email, presence: true, uniqueness: true
