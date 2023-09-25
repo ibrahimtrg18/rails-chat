a3979f7c9a4d5310e13de2b006751888482ba074
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2023-09-26 00:29:06 +0700

    fix: migration

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -48,9 +48,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_093746) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "password_digest"
   end
 
